daa4c7f3de474d4370cebe71016c65deb495a708
Andrew Esteves <esteves.andrew@gmail.com>
2021-07-26 22:19:51 +0100

    :rocket: First commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Build the binary"
+GOOS=linux GOARCH=amd64 go build -o main main.go
+
+echo "ZIP file"
+zip main.zip main
+
+echo "Cleaning up"
+rm main
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/facilittei/checkout-listener
+
+go 1.16
+
+require github.com/aws/aws-lambda-go v1.25.0 // indirect

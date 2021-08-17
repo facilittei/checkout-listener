6f046b7f94c26f66300ed11a1ae28ef04f4ff788
Andrew Esteves <esteves.andrew@gmail.com>
2021-08-17 19:22:43 +0100

    Log message to check that it is working

--- handlers/checkout.go
+++ handlers/checkout.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/facilittei/checkout-listener/services"
 )
@@ -20,5 +21,6 @@ func NewCheckout(paymentSrv services.PaymentContract) *Checkout {
 
 // Handle events requests
 func (checkoutHandler *Checkout) Handle(ctx context.Context, params interface{}) error {
+	fmt.Println("It worked!")
 	return nil
 }

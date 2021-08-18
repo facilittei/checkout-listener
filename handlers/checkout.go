98002307392f64180f822a9a7b866b85ad5eebf5
Andrew Esteves <esteves.andrew@gmail.com>
2021-08-18 20:10:29 +0100

    Logging messages

--- handlers/checkout.go
+++ handlers/checkout.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/facilittei/checkout-listener/gateways"
 	"github.com/facilittei/checkout-listener/services"
 )
@@ -29,6 +30,14 @@ func NewCheckout(messageGtw gateways.MessageContract, paymentSrv services.Paymen
 // Handle events requests
 func (checkoutHandler *Checkout) Handle(ctx context.Context, params interface{}) (string, error) {
 	log.Println(params)
+
+	if evt, ok := params.(*events.SQSEvent); ok {
+		log.Println(evt)
+		for _, message := range evt.Records {
+			log.Println(message)
+		}
+	}
+
 	payments := checkoutHandler.MessageGtw.GetPayments(params)
 	log.Println(payments)
 	return fmt.Sprintf("Version: %v", Version), nil

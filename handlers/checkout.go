01cc9c0b565818645396972b29c56ec8ef361b14
Andrew Esteves <esteves.andrew@gmail.com>
2021-08-18 19:40:30 +0100

    Printing received event message

--- handlers/checkout.go
+++ handlers/checkout.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/facilittei/checkout-listener/gateways"
 	"github.com/facilittei/checkout-listener/services"
@@ -27,7 +28,8 @@ func NewCheckout(messageGtw gateways.MessageContract, paymentSrv services.Paymen
 
 // Handle events requests
 func (checkoutHandler *Checkout) Handle(ctx context.Context, params interface{}) (string, error) {
+	log.Println(params)
 	payments := checkoutHandler.MessageGtw.GetPayments(params)
-	checkoutHandler.PaymentSrv.Process(payments[0])
+	log.Println(payments)
 	return fmt.Sprintf("Version: %v", Version), nil
 }

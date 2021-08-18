45ccdc1f6e1058e4c71fb1fe4b374571c13ffa1f
Andrew Esteves <esteves.andrew@gmail.com>
2021-08-18 20:01:21 +0100

    Checkout handler params type

--- handlers/checkout.go
+++ handlers/checkout.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"reflect"
 
 	"github.com/facilittei/checkout-listener/gateways"
 	"github.com/facilittei/checkout-listener/services"
@@ -29,6 +30,7 @@ func NewCheckout(messageGtw gateways.MessageContract, paymentSrv services.Paymen
 // Handle events requests
 func (checkoutHandler *Checkout) Handle(ctx context.Context, params interface{}) (string, error) {
 	log.Println(params)
+	log.Println(reflect.TypeOf(params))
 	payments := checkoutHandler.MessageGtw.GetPayments(&params)
 	log.Println(payments)
 	return fmt.Sprintf("Version: %v", Version), nil
